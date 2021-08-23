e450f8bb0e874d4df52173ad3a62676ed4f2a0dd
Casey Cole <coleca@email.sc.edu>
2021-08-23 19:44:28 -0400

    Added helloworld.cpp

--- lect2-basics.cpp
+++ lect2-basics.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <string>
 
+// A small comment
+/*
+* A large comment block
+*
+*/
+
+// using namespace std; //This is no good
+
 int main(int argc, char **argv) {
+    
     return 0; 
 }
