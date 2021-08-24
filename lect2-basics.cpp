379f065fcfeab7d251f16a6ded3259cfa878752d
Casey Cole <coleca@email.sc.edu>
2021-08-23 20:11:08 -0400

    Added cin statement

--- lect2-basics.cpp
+++ lect2-basics.cpp
@@ -10,6 +10,8 @@
 // using namespace std; //This is no good
 
 int main(int argc, char **argv) {
-    
+    int num = 0; 
+    std::cin >> num; 
+    std::cout << num << std::endl;
     return 0; 
 }
