928766fe6aa93526adcf17dbcd9a4e02594eb209
Casey Cole <coleca@email.sc.edu>
2021-08-24 11:17:28 -0400

    added more

--- lect2-basics.cpp
+++ lect2-basics.cpp
@@ -36,10 +36,9 @@ int main(int argc, char **argv) {
     // auto keyword for type inference
     auto i = 0;
     // std::cout << sizeof(i) << std::endl;
-    // cannot change types after that! 
+    // cannot change types after that!
     // i = 1.2;
     // std::cout << i << std::endl;
-    
     ///////////////////////////////////////////////////////////////////////
     // Arrays
     // type name[] = {init values};
