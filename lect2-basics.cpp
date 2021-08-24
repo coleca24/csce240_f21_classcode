9acd2c171bd928fb3d330f154d91f4fdc749f683
Casey Cole <coleca@email.sc.edu>
2021-08-24 11:04:58 -0400

    Fixed cpplint issues

--- lect2-basics.cpp
+++ lect2-basics.cpp
@@ -1,3 +1,4 @@
+// Copyright 2021 Casey A Cole
 #include <iostream>
 #include <string>
 
@@ -8,58 +9,55 @@
 */
 
 // using namespace std; //This is no good
-// typedef std::cout cout_std; 
+// typedef std::cout cout_std;
 
 int main(int argc, char **argv) {
-
     // I/O input and output to standard in/out (terminal)
-    int num = 0; 
-    std::cin >> num; 
+    int num = 0;
+    std::cin >> num;
     std::cout << num << std::endl;
 
     ///////////////////////////////////////////////////////////////////////
     // Basic data types
-    // int (4 bytes), float (4 bytes), char (1 byte), bool (1 byte), double (8 bytes)
-    // Different ways to initialize: 
-    int a(0), b(0); 
+    // int (4 bytes), float (4 bytes),
+    // char (1 byte), bool (1 bit), double (8 bytes)
+    // Different ways to initialize:
+    int a(0), b(0);
     int c = 0;
-    int d(0), e = 0; 
+    int d(0), e = 0;
 
-    bool flag = true; 
+    bool flag = true;
 
-    char p('p'); 
-    //std::cout << "Value of p is " << p << std::endl; 
+    char p('p');
+    // std::cout << "Value of p is " << p << std::endl;
     // What do you think this will be?
-    //std::cout << p + 1 << std::endl;
+    // std::cout << p + 1 << std::endl;
 
     // auto keyword for type inference
-    auto i = 0; 
-    //std::cout << sizeof(i) << std::endl; 
-    
+    auto i = 0;
+    // std::cout << sizeof(i) << std::endl;
     ///////////////////////////////////////////////////////////////////////
     // Arrays
     // type name[] = {init values};
     // type name[size] = {init value};
-    // type name[size1][size2]; 
+    // type name[size1][size2];
 
     ///////////////////////////////////////////////////////////////////////
     // if/else statements
-	// Ctrl / to toggle comments
-	// if() { 
-	    
-	// }
+    // Ctrl / to toggle comments
+    // if() {
+    // }
     // else if(){
 
     // }
-	// else{
-		
-	// }
+    // else{
+    // }
 
     ///////////////////////////////////////////////////////////////////////
     // switch statements
     // switch() {
     //     case something:
-    //         break; 
+    //         break;
     //     case something_else:
     //         break;
     //     default:
@@ -69,17 +67,17 @@ int main(int argc, char **argv) {
     // while loops
     // init step;
     // while(condition){
-    //     increment step; 
+    //     increment step;
     // }
 
     // Post vs. Pre?
 
     // do-while loops
-    // init step; 
-	// do{
-	// 	//do something
+    // init step;
+    // do{
+    //    //do something
     //     increment step;
-	// }while(condition);
+    // }while(condition);
 
     ///////////////////////////////////////////////////////////////////////
     // for loops
@@ -87,5 +85,5 @@ int main(int argc, char **argv) {
     //     do something
     // }
 
-    return 0; 
+    return 0;
 }
