de4d87a2bda9180563fa5b00914070c1c943884e
Casey Cole <coleca@email.sc.edu>
2021-08-24 12:03:48 -0400

    003 - helloworld

--- lect2-helloworld.cpp
+++ lect2-helloworld.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+// using namespace std;
+
 int main(int argc, char **argv) {
-    std::cout << "Hello World" << std::endl; 
-    return 0; 
+    std::cout << "Hello World" << std::endl;
+    return 0;
 }
+
