6d251c16aabeeaf437254b8160b0d226da21c66e
Casey Cole <coleca@email.sc.edu>
2021-08-31 10:20:22 -0400

    lect4 base code added

---
+++ lect4-ndArray-streams.cpp
@@ -0,0 +1,31 @@
+// Copyright 2021 Casey Cole
+#include <iostream>
+#include <sstream>
+
+using std::string;
+using std::cout;
+
+// Function prototypes go here
+// void print2dArray(int arr[][]);
+
+int main(int argc, char **argv) {
+    // ND Arrays in Functions
+    int arr[2][2] = {{1, 1}, {2, 2}};
+
+    // Cin and getline()
+    // https://www.cplusplus.com/reference/string/string/getline/
+
+    // Stringsteams
+
+    // printf - The pretty way to print
+    // https://www.cplusplus.com/reference/cstdio/printf/
+    // printf("hello %s", "Casey");
+    string str = "hello";
+    // Reading from a file
+
+    // Writing to a file
+
+
+
+    return 0;
+}
