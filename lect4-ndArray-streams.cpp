f8a015a4f3f40f80dcfee3b6b8f625f57ca08032
Casey Cole <coleca@email.sc.edu>
2021-08-31 10:46:59 -0400

    Added some more to lect4 base code

--- lect4-ndArray-streams.cpp
+++ lect4-ndArray-streams.cpp
@@ -16,16 +16,21 @@ int main(int argc, char **argv) {
     // https://www.cplusplus.com/reference/string/string/getline/
 
     // Stringsteams
+    // Exercise: Read in a string with a number in it and
+    // use a stringstream to parse out the number.
+
+    // I wonder what happens if there are two numbers...
 
     // printf - The pretty way to print
     // https://www.cplusplus.com/reference/cstdio/printf/
     // printf("hello %s", "Casey");
-    string str = "hello";
+
     // Reading from a file
 
     // Writing to a file
 
-
+    // Exercise: What if the file is CSV? 
+    // More on files: https://www.cplusplus.com/doc/tutorial/files/
 
     return 0;
 }
