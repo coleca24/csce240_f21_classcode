f0e1385a6fb073af4e7af0f67c29b0d7b307050c
Casey Cole <coleca@email.sc.edu>
2021-09-07 16:06:04 -0400

    added edits for 001

--- 001/lect5-introPointers.cpp
+++ 001/lect5-introPointers.cpp
@@ -2,16 +2,15 @@
 #include <iostream>
 #include <sstream>
 
-using std::string;
 using std::cout;
+using std::string;
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // Introduction to pointers:
     int x(0);
     x = 100;
     // cout << x << "\n";
-
     int *xPtr;
     xPtr = &x;
 
@@ -24,17 +23,31 @@ int main(int argc, char **argv) {
 
     // Where is this useful?
     // Use case: resizing arrays
-    int arr[5] = {1,1,1,1,1}; 
-    // We now want: {1,1,1,1,1,10}
-    // Let's attempt with primative arrays
-
+    // int arr[5] = {1, 1, 1, 1, 1};
+    // // We now want: {1,1,1,1,1,10}
+    // // Let's attempt with primative arrays
+    // int arr1[6];
+    // for (int i = 0; i < 5; i++) {
+    //     arr1[i] = arr[i];
+    // }
+    // arr[5] = 100;
     // So what's the big problem? It works
 
-
-
     // 1) Consistant naming
-    // 2) Space 
+    // 2) Space
     // More reading: https://www.geeksforgeeks.org/memory-layout-of-c-program/
 
     // How to do it with pointers?
+    int *arr;
+    arr = new int[5];
+    for (int i = 0; i < 5; i++) {
+        arr[i] = 1;
+    }
+    int *temp; 
+    temp = arr;
+    arr = new int[6];
+    // copy values over
+    arr[5] = 10;
+    delete [] temp;
+    delete [] arr;
 }
--- 001/lect5-misc-p1.cpp
+++ 001/lect5-misc-p1.cpp
@@ -9,12 +9,25 @@ int main(int argc, char **argv) {
     // Who completed the challenge?
     // Reading in a csv file into a 2d array?
     // Hint:
-    string line = "10,10";
+    string line = "10,11";
+    string token = "";
     std::stringstream s_line(line);
     // getline()!
 
+    // while (getline(s_line, token, ',') )
+    // cout << token << "\n";
+
     // genPassword() in P1... how to generate random
     // chars? Ideas?
+    srand(time(NULL));
+    cout << time(NULL) << "\n";
     int randNum = 0;
+    for (int i =0; i <5; i++) {
+        randNum = rand() % 10 + 48;
+        char c = randNum;
+        cout << randNum << "\n";
+        cout << c << "\n";
+    }
+    
     return 0;
 }
