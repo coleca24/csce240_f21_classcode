e0c5df6819c8529bb3e1e74850288960de29d75f
Casey Cole <coleca@email.sc.edu>
2021-09-07 11:04:28 -0400

    added other

---
+++ other/test.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	       System.out.println(return1()); 	
+	}
+	public static int return1() {
+		return 1;
+	}
+}
