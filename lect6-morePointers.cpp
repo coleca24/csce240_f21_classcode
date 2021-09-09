422c34bc5bb0524f9a0fe6adab795b495136cfed
Casey Cole <coleca@email.sc.edu>
2021-09-09 10:42:34 -0400

    added lecture 6 template

---
+++ lect6-morePointers.cpp
@@ -0,0 +1,27 @@
+// Copyright 2021 Casey Cole
+#include <iostream>
+
+using std::cout;
+
+
+int main(int argc, char **argv) {
+    // Pointer review: Just a variable that holds a memory location!
+    // Some legal initializations of a pointer variable
+
+    // Some illegal initializations
+
+    // The new and delete keyword
+    
+    // new and delete with arrays
+
+    // Pointers in functions?
+    // As parameters
+    // printArray(ptr, size);
+
+
+    // As a return
+    // int * oldArray = new int[5];
+    // int * newArry;
+    // newArray = resizeArray(oldArray, size);
+
+}
