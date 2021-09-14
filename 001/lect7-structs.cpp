4ed518045e5b30b688ea5d2b9a30de485406c9e6
Casey Cole <coleca@email.sc.edu>
2021-09-14 14:38:20 -0400

    added lect7 base

---
+++ 001/lect7-structs.cpp
@@ -0,0 +1,42 @@
+// Copyright 2021 Casey A Cole
+#include <iostream>
+#include <string>
+
+using std::string;
+
+// Basically an object with no functions and everything is public.
+// struct book {
+
+// };
+
+// TODO(class): add initBook function to get in the data from the user.
+
+// TODO(class): add printCollection function.
+
+// TODO(class): add addBook function that will add a new book to the
+// collection.
+
+// TODO(on your own): add a removeBook function that will search for
+// a title and remove that from the collection of structs.
+
+int main(int argc, char **argv) {
+    // Creating a new book (struct)
+
+    // Using curly braces: = {val1, val2};
+
+    // What is the size of this struct?
+
+    // Manual initialization of each property
+
+    // Create the initBook function.
+
+    // Static allocation of book collection
+
+    // Create printCollection() function
+
+    // Dynamic allocation of book collection with pointers
+
+    // addBook() function
+
+    return 0;
+}
