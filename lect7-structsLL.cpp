66726bed7d10a4ddbf1df1d474c69f9cb324d0d9
Casey Cole <coleca@email.sc.edu>
2021-09-14 10:52:22 -0400

    added lecture 7 base code

---
+++ lect7-structsLL.cpp
@@ -0,0 +1,20 @@
+// Copyright 2021 Casey A Cole
+#include <iostream>
+
+struct node {
+    int data;
+    node *next;
+};
+
+int main(int argc, char **argv) {
+    node head;
+    node tail;
+
+    // Let's connect the two
+
+    // Now let's walk through manually
+
+    // Question: How would I make this a doubly linked list?
+
+    return 0;
+}
