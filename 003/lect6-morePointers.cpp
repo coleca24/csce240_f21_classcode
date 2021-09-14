9d732f488ba0863c34e82334c0f4e5ac4feb6a24
Casey Cole <coleca@email.sc.edu>
2021-09-14 13:15:57 -0400

    added 003 changes

--- 003/lect6-morePointers.cpp
+++ 003/lect6-morePointers.cpp
@@ -9,67 +9,74 @@ void resize(int *, int);
 int * append(const int *, int, int);
 
 int main(int argc, char **argv) {
-    // Pointer review: Just a variable
-    // that holds a memory location!
-    // Some legal initializations of a pointer variable
-    int x = 0;
-    // int *xp = &x;
+    // // Pointer review: Just a variable
+    // // that holds a memory location!
+    // // Some legal initializations of a pointer variable
+    // int x = 0;
+    // // int *xp = &x;
 
-    // int arr[10];
-    // xp = arr;
-    // xp = new int;
-    // xp = new int[10];
+    // // int arr[10];
+    // // xp = arr;
+    // // xp = new int;
+    // // xp = new int[10];
     
-    // Some illegal initializations
-    // xp = x;
-    // xp = new float;
+    // // Some illegal initializations
+    // // xp = x;
+    // // xp = new float;
 
-    // The new and delete keyword
-    int *xp = new int;
-    *xp = 10;
-    delete xp;
+    // // The new and delete keyword
+    // int *xp = new int;
+    // *xp = 10;
+    // delete xp;
 
-    // new and delete with arrays
-    xp = new int[5];
-    int **xp2;
-    xp2 = new int*[5];
-    for (int i = 0; i < 5; i++) {
-        // if (i == 2) {
-        //     xp2[i] = new int[3];
-        // } else {
-            xp2[i] = new int[2];
-        // }
-    }
-    for (int i = 0; i < 5; i++) {
-        delete [] xp2[i];
-    }
-    delete [] xp2;
+    // // new and delete with arrays
+    // xp = new int[5];
+    // delete [] xp;
+    // int **xp2;
+    // xp2 = new int*[5];
+    // for (int i = 0; i < 5; i++) {
+    //     // if (i == 2) {
+    //     //     xp2[i] = new int[3];
+    //     // } else {
+    //         xp2[i] = new int[2];
+    //     // }
+    // }
+    // for (int i = 0; i < 5; i++) {
+    //     delete [] xp2[i];
+    // }
+    // delete [] xp2;
 
-    // Pointers in functions?
-    // As parameters
-    int *ptr = new int[5];
-    // cout << sizeof(*ptr) << "\n";
-    for (int i = 0; i < 5; i++) ptr[i] = 1;
-    int size = 5;
-    printArray(ptr, size);
-    for (int i = 0; i < size; i++) {
-        cout << ptr[i] << " ";
-    }
-    cout << "\n";
-    // Let's examine if we are doing call-by-ref
-    // or call-by-value
-    cout << "location of &ptr in main " << &ptr << "\n";
-    resize(ptr, 6);
-    for (int i = 0; i < 6; i++) {
-        cout << ptr[i] << " ";
-    }
-    cout << "\n";
-    // As a return
-    int * oldArray = new int[5];
-    int * newArry;
-    newArry = append(oldArray, size, 10);
-    printArray(newArry, 6);
+    // // Pointers in functions?
+    // // As parameters
+    // int *ptr = new int[5];
+    // // cout << sizeof(*ptr) << "\n";
+    // for (int i = 0; i < 5; i++) ptr[i] = 1;
+    // int size = 5;
+    // printArray(ptr, size);
+    // for (int i = 0; i < size; i++) {
+    //     cout << ptr[i] << " ";
+    // }
+    // cout << "\n";
+    // // Let's examine if we are doing call-by-ref
+    // // or call-by-value
+    // cout << "location of &ptr in main " << &ptr << "\n";
+    // // resize(ptr, 6);
+    // // for (int i = 0; i < 6; i++) {
+    // //     cout << ptr[i] << " ";
+    // // }
+    // // cout << "\n";
+    // // As a return
+    // int * oldArray = new int[5];
+    // // YOU DID NOT INITIALIZE -.-
+    // int * newArry;
+    // newArry = append(oldArray, size, 10);
+    // printArray(newArry, 6);
+    // delete [] oldArray;
+    // delete [] newArry;
 
+    int *temp = new int[2];
+    // temp[2] = 10;
+    delete [] temp;
     // What should we do for memory clean up?
 
 }
--- 003/lect7-structs.cpp
+++ 003/lect7-structs.cpp
@@ -5,10 +5,21 @@
 using std::string;
 
 // Basically an object with no functions and everything is public.
-// struct book {
+struct book {
+    string title;
+    string author;
+    float price;
+    int words;
+    string genre;
+};
 
-// };
+// printBook
+void printBook(book);
+// initBook(b1);
+void initBook(book&);
 
+// addBook(collection2, b2, size)
+book* addBook(const book*, book, int&);
 // TODO(class): add initBook function to get in the data from the user.
 
 // TODO(class): add printCollection function.
@@ -18,23 +29,72 @@ using std::string;
 
 // TODO(on your own): add a removeBook function that will search for
 // a title and remove that from the collection of structs.
+// remove last book
+book* removeBook(book*, int& size);
+// remove at given index
+book* removeBook(book*, int index, int& size);
 
+using std::cout;
 int main(int argc, char **argv) {
     // Creating a new book (struct)
-
+    book b1;
     // What is the size of this struct?
-
+    // cout << sizeof(b1) << "\n";
     // Manual initialization of each property
-
+    b1.words = 100;
+    // cout << b1.words << "\n";
+    book b2 = {"title", "author", 10.99, 10000, "fiction"};
+    // cout << b2.title << "\n";
+    printBook(b2);
     // Create the initBook function.
-
+    initBook(b1);
+    printBook(b1);
     // Static allocation of book collection
-
+    book collection[3];
     // Create printCollection() function
-
+    // for (int i = 0; i < 3; i++) {
+    //     // collection[i].author;
+    // }
     // Dynamic allocation of book collection with pointers
-
+    book *collection2;
+    collection2 = new book[0];
     // addBook() function
+    int size = 0;
+    collection2 = addBook(collection2, b2, size);
+    collection2 = addBook(collection2, b2, size);
+    collection2 = addBook(collection2, b2, size);
 
+    delete [] collection2;
     return 0;
+}
+
+void printBook(book b) {
+    printf("Title: %s\n", b.title.c_str());
+    printf("Author: %s\n", b.author.c_str());
+    printf("Price: %f\n", b.price);
+    printf("Words: %i\n", b.words);
+    printf("Genre: %s\n", b.genre.c_str());
+}
+void initBook(book& b) {
+    getline(std::cin, b.title, ',');
+    getline(std::cin, b.author, ',' );
+    string token;
+    getline(std::cin, token, ',' );
+    b.price = stof(token);
+    getline(std::cin, token, ',' );
+    b.words = stoi(token);
+    getline(std::cin, b.genre, ',' );
+    // std::cin >
+}
+
+book* addBook(const book* col, book b, int& size) {
+    book * ret = new book[size+1];
+    for (int i = 0; i < size; i++) {
+        // ret[i] = col[i];
+        ret[i].title = col[i].title;
+    }
+    delete [] col;
+    ret[size].title = b.title;
+    size++;
+    return ret;
 }
