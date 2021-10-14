6902401b7f87dfefe19a3d373ff12a6a209f11dc
Casey Cole <coleca@email.sc.edu>
2021-10-14 14:21:04 -0400

    changes for 003

--- 003/lect14-inheritance/vehicles.cpp
+++ 003/lect14-inheritance/vehicles.cpp
@@ -17,21 +17,33 @@ using std::endl;
 class Vehicle {
  public:
     Vehicle() {
-        numberWheels = 4;
+        
+        numberWheels = 10;
     }
-    void print() {
+    explicit Vehicle(int nw) {
+        cout << "vehicle\n";
+        numberWheels = nw;
+    }
+    virtual void print() {
         cout << "Number of Wheels: " << numberWheels << endl;
     }
+    virtual void print() = 0;
  private:
+//  protected:
     int numberWheels;
 };
 
 class Car : public Vehicle {
  public :
-        Car() {
+        Car() : Vehicle(4) {
+            cout << "car\n";
             mpg = 20.0;
         }
+        Car(float mpg, int nw) : Vehicle(nw) {
+            this->mpg = mpg;
+        }
         void print() {
+            // cout << "Vehicle: " << numberWheels << endl;
             Vehicle::print();
             cout << "MPG: " << mpg << endl;
         }
@@ -39,14 +51,31 @@ class Car : public Vehicle {
         float mpg;
 };
 
-int main(int argc, char** argv) {
-    Car c1;
-    c1.print();
+class Battery {
 
+};
+
+class Tesla : public Car, public Battery {
+ public:
+    void print() {
+        Car::print();
+        cout << "Range: " << range << "\n";
+    }
+ private:
+    float range;
+};
+
+int main(int argc, char** argv) {
+    // Car c1;
+    // c1.print();
+    // Tesla t;
+    // t.print();
+
+    Car c1;
     // Vehicle v1 = c1;
     // v1.print();
 
-    // Vehicle *v1 = &c1;
-    // v1->print();
+    Vehicle *v1 = &c1;
+    v1->print();
     return 0;
 }
