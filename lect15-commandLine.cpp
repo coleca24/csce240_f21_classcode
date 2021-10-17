ad847d267b64e70d51bd1eb88724a6571fb7bb72
Casey Cole <coleca@email.sc.edu>
2021-10-17 18:00:00 -0400

    lect 15 base code

---
+++ lect15-commandLine.cpp
@@ -0,0 +1,17 @@
+// Copyright 2021 Casey A Cole
+#include <iostream>
+
+int main(int argc, char **argv) {
+    // Print out the number of arguments (argc)
+
+    // Usage statement
+
+    // print out one of the arguments argv[0]...argv[n]
+
+    // loop through all the arguments
+
+    // Remember argv is a char array.
+    // We need to convert it to an integer... (atoi)
+
+    return 0;
+}
---
+++ lect15-exceptions.cpp
@@ -0,0 +1,45 @@
+// Copyright 2021 Casey A Cole
+#include <iostream>
+#include <exception>
+
+bool validateNum(int);
+
+int main(int argc, char **argv) {
+    if (argc < 4) {
+        std::cout << "Usage: ./a.out <number 1> <operation> <number 2>" << "\n";
+        exit(1);
+    }
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[3]);
+    char op = argv[2][0];
+    try {
+        if (op == '+') {
+            std::cout << num1 + num2 << "\n";
+        } else if (op == '-') {
+            std::cout << num1 - num2 << "\n";
+        } else if (op == '*') {
+            std::cout << num1 * num2 << "\n";
+        } else if (op == '/') {
+            std::cout << num1 / num2 << "\n";
+        }
+    } catch(std::exception e) {
+        std::cout << e.what() << "\n";
+    }
+
+    // What about validating something more specific, like if the numbers are 
+    // above a certain value
+
+    return 0;
+}
+
+bool validateNum(int num) {
+    try {
+        if (num > 10) {
+            throw num; 
+        }
+        return true;
+    } catch (int e) {
+        std::cout << e << " was greater than 10\n";
+        return false;
+    }
+}
