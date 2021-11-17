cc9a8a3ce1c56d60d29d3f9fe09217ff992449aa
Casey Cole <coleca@email.sc.edu>
2021-11-17 15:27:07 -0500

    lect 22

---
+++ 002/lect22-lambda-ranges-views.cpp
@@ -0,0 +1,128 @@
+// Copyright 2021 Casey A Cole
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+// #include <ranges>
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::for_each;
+
+// Structure of lambda
+// [ capture clause ] (parameters) -> return-type
+// {
+//    definition of method
+// }
+
+void myfunction(int i) {
+    cout << i << " ";
+    // i = i+1;
+}
+
+void squarefunction(int & i) {
+    i = i*i;
+}
+
+int main(int argc, char **argv) {
+    vector<int> v{2, 4, 6, 10, 2, 57};
+    // Printing
+    // for (int &i : v) {
+    //     cout << i << " ";
+    //     // i = i + 1;
+    // }
+    // cout << "\n";
+
+    // for (int i : v) {
+    //     cout << i << " ";
+    // }
+    // cout << "\n";
+
+    // for_each in algorithm
+    // https://www.cplusplus.com/reference/algorithm/
+    // https://www.cplusplus.com/reference/algorithm/for_each/
+    for_each(v.begin(), v.end(), myfunction);
+    cout << "\n";
+    for_each(v.begin(), v.end(), squarefunction);
+    // cout << "\n";
+    for_each(v.begin(), v.end(), myfunction);
+    cout << "\n";
+
+    // for (int i : v) {
+    //     cout << i << " ";
+    // }
+    // cout << "\n";
+
+// Structure of lambda
+// [ capture clause ] (parameters) -> return-type
+// {
+//    definition of method
+// }
+    // Printing with Lambda
+    for_each(v.begin(), v.end(), [](int & i) {
+        // std::cout << i << " ";
+        i = i + 1;
+    });
+    cout << "\n";
+
+    for_each(v.begin(), v.end(), myfunction);
+    cout << "\n";
+    // for (int i : v) {
+    //     cout << i << " ";
+    // }
+    // cout << "\n";
+
+    // Another application... How would you go about
+    // finding the first element that is greater than a certain number?
+    int index = -1;
+    vector<int>::iterator p = find_if(v.begin(), v.end(), [&index](const int &i) {
+        index++;
+        return i > 6 && i != 17;
+        // Add more logic?
+    });
+    cout << *p << "\n";
+    cout << index << "\n";
+
+    int count = count_if(v.begin(), v.end(), [](int i) {
+        return i > 6 && i != 17;
+        // Add more logic?
+    });
+    cout << count << "\n";
+
+    int value = 6;
+    auto myFunction = [&]() {
+        value = value*2;
+        return value*2;
+    };
+    cout << myFunction() << "\n";
+    cout << value << "\n";
+
+    std::vector<int> vec { 0, 5, 2, 9, 7, 6, 1, 3, 4, 8 };
+
+    int compCounter = 0;
+    std::sort(vec.begin(), vec.end(), [&compCounter](int a, int b) {
+        ++compCounter;
+        return a < b;
+    });
+
+    // auto const ints = {0,1,2,3,4,5};
+    // auto even = [](int i) { return 0 == i % 2; };
+    // auto square = [](int i) { return i * i; };
+ 
+    // // // // "pipe" syntax of composing the views:
+    // for (int i : ints | std::views::filter(even) | std::views::transform(square)) {
+    //     std::cout << i << ' ';
+    // }
+ 
+    // std::cout << '\n';
+ 
+    // // a traditional "functional" composing syntax:
+    // for (int i : std::views::transform(std::views::filter(ints, even), square)) {
+    //     std::cout << i << ' ';
+    // }
+    return 0;
+}
+
+// Learn more: https://docs.microsoft.com/en-us/cpp/cpp/lambda-expressions-in-cpp?view=msvc-170
+// More examples: https://en.cppreference.com/w/cpp/ranges
