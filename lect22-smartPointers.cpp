555da61f5864ff4f08b09769954a76b4813c0178
coleca24 <coleca@email.sc.edu>
2021-11-18 08:57:26 -0500

    Create lect22-smartPointers.cpp

---
+++ lect22-smartPointers.cpp
@@ -0,0 +1,41 @@
+// Copyright 2021 Casey A Cole
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+// #include <ranges>
+#include <memory>
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::for_each;
+
+void fun() {
+    int *pt = new int;
+}
+
+
+int main(int argc, char **argv) {
+    // Recall pointers - 
+    // Points to a new int that is stored in the heap (dynamically allocated)
+    int *pt = new int; 
+    // At this point, we have a memory leak because we have not deallocated the space that we allocated. 
+    // This gets even more pronouced when we use pointers in functions. 
+    while (true) {
+        fun();
+    }
+    // And where would we deallocate this? Assuming that we need the int that pt is pointing to... we can't do it in the function... 
+    // Enter smart pointers (#include <memory>) 
+    // Two main types: 
+    // 1 - unique pointer
+    std::unique_ptr<int> pt2 = std::make_unique<int>(20);
+    cout << *pt2 << "\n";
+    // 2 - shared pointer - two pointers can point to the same spot, and there is a reference counter
+    std::shared_ptr<int> pt3 = std::make_unique<int>(100);
+    cout << *pt3 << "\n";
+    std::shared_prt<int> pt4; 
+    pt4 = pt3;
+  
+    return 0;
+}
