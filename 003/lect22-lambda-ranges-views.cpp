e06ba8d597811dcd14e1921c3d09f450b147ef57
Casey Cole <coleca@email.sc.edu>
2021-11-18 12:28:02 -0500

    Changes for 003 lecture 22

---
+++ 003/lect22-lambda-ranges-views.cpp
@@ -0,0 +1,113 @@
+// Copyright 2021 Casey A Cole
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <ranges>
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::for_each;
+
+// Structure of lambda
+// [ capture clause ] (parameters) -> return-type
+// {
+//    definition of method
+// }
+
+void myfunction(int i) {
+    cout << i << " ";
+    // i = i+1;
+}
+
+int main(int argc, char **argv) {
+    vector<int> v{2, 4, 6, 10, 2, 57};
+    // Printing
+    // for (int i : v) {
+    //     cout << i << " ";
+    // }
+    // cout << "\n";
+
+    // for_each in algorithm
+    // https://www.cplusplus.com/reference/algorithm/for_each/
+    // for_each(v.begin(), v.end(), myfunction);
+    // cout << "\n";
+
+    // for (int i : v) {
+    //     cout << i << " ";
+    // }
+    // cout << "\n";
+
+    // Printing with Lambda
+    // for_each(v.begin(), v.end(), [](int & i) {
+    //     // std::cout << i << " ";
+    //     i = i+1;
+    // });
+    // cout << "\n";
+
+    // for (int i : v) {
+    //     cout << i << " ";
+    // }
+    // cout << "\n";
+
+    // Another application... How would you go about
+    // finding the first element that is greater than a certain number?
+    // vector<int>::iterator p = find_if(v.begin(), v.end(), [](int i) {
+    //     return i > 6 && i != 7;
+    //     // Add more logic?
+    // });
+    // cout << *p << "\n";
+
+    // int count = count_if(v.begin(), v.end(), [](int i) {
+    //     return i > 6 && i != 7;
+    //     // Add more logic?
+    // });
+    // cout << count << "\n";
+
+    int value = 6;
+    int num = 2;
+    auto myFunction = [&value, num] () -> int{
+        value = value*num;
+        return value;
+    };
+    cout << myFunction() << "\n";
+    cout << value << "\n";
+
+    for(int i : v) {
+        cout << i << " ";
+    }
+    cout << "\n";
+
+    int basic_op = 0;
+    sort(v.begin(), v.end(), [&basic_op] (int i, int j) {
+        basic_op++;
+        return i < j;
+    });
+    cout << basic_op << "\n";
+    for(int i : v) {
+        cout << i << " ";
+    }
+    cout << "\n";
+
+    auto const ints = {0,1,2,3,4,5};
+    auto even = [](int i) { return 0 == i % 2; };
+    auto square = [](int i) { return i * i; };
+ 
+    // // "pipe" syntax of composing the views:
+    for (int i : ints | std::views::filter(even) |
+                        std::views::transform(square)) {
+        std::cout << i << ' ';
+    }
+ 
+    std::cout << '\n';
+ 
+    // a traditional "functional" composing syntax:
+    for (int i : std::views::transform(std::views::filter(ints, even), square)) {
+        std::cout << i << ' ';
+    }
+    return 0;
+}
+
+// Learn more: https://docs.microsoft.com/en-us/cpp/cpp/lambda-expressions-in-cpp?view=msvc-170
+// More examples: https://en.cppreference.com/w/cpp/ranges
--- 003/lect22-smartPointers.cpp
+++ 003/lect22-smartPointers.cpp
@@ -34,14 +34,22 @@ int main(int argc, char **argv) {
     // Enter smart pointers (#include <memory>)
     // Two main types:
     // 1 - unique pointer
+    // int *temp = data;
+    // int * pt2 = new int;
+    // *pt2 = 20;
     std::unique_ptr<int> pt2 = std::make_unique<int>(20);
     cout << *pt2 << "\n";
+    std::unique_ptr<int> p5;
+    // p5 = pt2;
+
     // 2 - shared pointer - two pointers can
     // point to the same spot, and there is a reference counter
-    std::shared_ptr<int> pt3 = std::make_unique<int>(100);
+    std::shared_ptr<int> pt3 = std::make_shared<int>(100);
     cout << *pt3 << "\n";
     std::shared_ptr<int> pt4;
     pt4 = pt3;
     cout << *pt4;
+
+    // 3 - weak pointer
     return 0;
 }
